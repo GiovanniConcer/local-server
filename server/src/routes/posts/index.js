bdef826dd47896f7bf7dc52d7626a604d821af59
Giovanni Concer <giovanniconcer@hotmail.com>
2023-10-28 13:01:21 +0100

    added get and post endpoints

--- server/src/routes/posts/index.js
+++ server/src/routes/posts/index.js
@@ -1,8 +1,43 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (request, response) => {
-    response.send("post funfou")
+// Models
+const {Posts} = require('../../database')
+
+async function getAllPosts() {
+    const posts = await Posts.find()
+    return posts.map((post)=>{
+        return {
+            id: post._id,
+            title: post.title,
+            body: post.body
+        }
+    })
+}
+
+router.get('/', async (request, response) => {
+    const posts = await getAllPosts()
+    response.send(posts)
+})
+
+router.post('/', (request, response) => {
+    try {
+        const {title, body} = request.body
+        if (!title) throw Error('missing title')
+        if (!body) throw Error('missing body')
+        const newPost = new Posts(body)
+        newPost.save()
+        response.send({
+            timestamp: Date.now(),
+            message: 'Post created',
+            data: newPost
+        })
+    } catch(error) {
+        response.status(400).send({
+            timestamp: Date.now(),
+            message: error.message
+        })
+    }
 })
 
 module.exports = router
