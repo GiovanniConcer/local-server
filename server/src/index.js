76f04f67c17e88d8e9c05cd3f978c1898928bf72
Giovanni Concer <giovanniconcer@hotmail.com>
2023-10-28 10:44:44 +0100

    added posts route

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,16 @@
-const express = require("express");
+const express = require('express')
+const posts = require('./routes/posts')
+
+// Server config
 const app = express()
 
 const PORT = 3000;
 
 app.listen(PORT, function() {
-    console.log(`Server listening ${PORT}`);
+    console.log(`Server listening ${PORT}`)
 })
 
-app.use(express.static("../../client"))
+app.get('/', (request, response) => response.send('test'))
+
+// Routes
+app.use('/posts', posts)
